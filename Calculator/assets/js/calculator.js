47d55b61263b9c66c9c1890f92e773d62d792b3f
sintayehu-B <61502710+sintayehu-B@users.noreply.github.com>
2021-01-31 20:35:27 +0300

    fixed functions

--- Calculator/assets/js/calculator.js
+++ Calculator/assets/js/calculator.js
@@ -1,99 +1,149 @@
-// returns the history value
-function getHistory(){
-    return document.getElementById('history-value').innerText;
+
+function subtraction(){
+    let number1 = prompt('the first Number ');
+    let number2 = prompt('the Second Number');
+    
+    let subtract = number1 - number2;
+
+    return subtract;
 
 }
-// alert(getHistory())
-// prints history
-function printHistory(num){
-    document.getElementById('history-value').innerText=num;
 
-}
-// printHistory("9 * 9 + 81");
-// returns the output of the value
-function getOutput(){
-    return document.getElementById('output-value').innerText;
-}
-// alert(getOutput());
-
-// prints the output 
-function printOutput(num){
-    if (num == "") {
-        document.getElementById('output-value').innerText = num
+function division(){
+    let number1 = prompt('The first number');
+    let number2 = prompt('The second number');
+    if (number1 <= 0 ) {
+        console.log("Can't be divided by zero");
     }else{
-        document.getElementById('output-value').innerText = 
-        getFormattedNumber(num);
-    }
-}
-
-// Reads the number and returns coma separated number 
-function getFormattedNumber(num){
-
-    if (num == "-") {
-        return ""
         
+        let divide = number1 / number2;
+        
+        return divide;
     }
-    let number = Number(num);
-    let value = number.toLocaleString("en")
-    return value
-}
-// printOutput("45333333333333")
 
-//  reverse number format
-function reversNumberFormat(num){
-    return Number(num.replace(/,/g, ''))
-}
-// alert(reversNumberFormat(getOutput()))
-
-var operator = document.getElementsByClassName('operator');
-for (let i = 0; i < operator.length; i++) {
-    operator[i].addEventListener('click', function(){
-        if (this.id == "clear") {
-            printHistory("");
-            printOutput("");
-        }
-        else if (this.id == "backspace") {
-            let output = reversNumberFormat(getOutput()).toString();
-            if(output){
-                output = output.substr(0, output.length - 1)
-                printOutput(output);
-            }
-        }else{
-			var output=getOutput();
-			var history=getHistory();
-			if(output==""&&history!=""){
-				if(isNaN(history[history.length-1])){
-					history= history.substr(0,history.length-1);
-				}
-			}
-			if(output!="" || history!=""){
-                output= output==""?
-                output:reverseNumberFormat(output);
-				history=history+output;
-				if(this.id=="="){
-					var result=eval(history);
-					printOutput(result);
-					printHistory("");
-				}
-				else{
-					history=history+this.id;
-					printHistory(history);
-					printOutput("");
-				}
-			}
-        }
-    })
     
 }
-var number = document.getElementsByClassName('number');
-for (let i = 0; i < number.length; i++) {
-    number[i].addEventListener('click', function(){
-        let output = reversNumberFormat(getOutput());
-        if(output != NaN){
-            output = output + this.id
-            printOutput(output);
-            
-        }
-    })
+
+function square(){
+    let number = prompt("a number to square: ")
+    return number * number;
+}
+
+function addition(){
+    let inputNumber = [];
+    let input = prompt('how many number to add');
+    result = 0;
+    for (let i = 0; i < parseInt(input); i++) {
+        inputNumber[i] = prompt(" the number " + (i + 1));
+    }
+    
+    for(i in inputNumber){
+        result += parseInt(inputNumber[i])
+    }
+    return result
+
     
 }
+function multiplication(){
+    let inputNumber = [];
+    let input = prompt('how many number to add');
+    result = 1;
+    for (let i = 0; i < parseInt(input); i++) {
+        inputNumber[i] = prompt(" the number " + (i + 1));
+    }
+    console.log(inputNumber);
+    for(i in inputNumber){
+        result *= parseInt(inputNumber[i])
+    }
+    return result
+
+    
+}
+// function getHistory(){
+// 	return document.getElementById("history-value").innerText;
+// }
+// function printHistory(num){
+// 	document.getElementById("history-value").innerText=num;
+// }
+// function getOutput(){
+// 	return document.getElementById("output-value").innerText;
+// }
+// function printOutput(num){
+// 	if(num==""){
+// 		document.getElementById("output-value").innerText=num;
+// 	}
+// 	else{
+// 		document.getElementById("output-value").innerText=getFormattedNumber(num);
+// 	}	
+// }
+function number1(){
+    document.getElementById("history").innerText ="1"
+
+}
+function number2(){
+    return document.result.input_value.value += "1"
+
+}
+function number3(){
+    return document.Keyboard.input_value.value += "1"
+
+}
+function number4(){
+    return document.Keyboard.input_value.value += "1"
+
+}
+function number5(){
+    return document.Keyboard.input_value.value += "1"
+
+}
+function number6(){
+    return document.Keyboard.input_value.value += "1"
+
+}
+function number7(){
+    return document.Keyboard.input_value.value += "1"
+
+}
+function number8(){
+    return document.Keyboard.input_value.value += "1"
+
+}
+function number9(){
+    return document.Keyboard.input_value.value += "1"
+
+}
+function number0(){
+    return document.Keyboard.input_value.value += "1"
+
+}
+function clear(){
+    document.Keyboard.input_value.value = " ";
+
+}
+function number1(num){
+    document.keyboard.input_value.value = num
+
+}
+function dot(){
+    document.keyboard.input_value.value += 1
+
+}
+
+// (function invoke(){
+//     let choice = parseInt(prompt('choose between the 5 functions \n 1 for Addition, \n  2 for subtraction, \n  3.for division, \n  4 for multiplication \n and 5 for squaring'))  ;
+//     switch(choice){
+//         case 1: console.log(addition());
+//         break;
+//         case 2: console.log(subtraction());
+//         break;
+//         case 3: console.log(division());
+//         break;
+//         case 4: console.log(multiplication());
+//         break;
+//         case 5: console.log(square());
+//         break;
+//         default: console.log('Wrong input try to insert 1-5 please');
+//         invoke();
+        
+//     }
+// })();
